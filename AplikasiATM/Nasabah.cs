318c6475ebcdf8c3fb799fd4d05e8bbd803ebf3a
Muhammad Natsir <elmunats@outlook.com>
2016-07-18 11:48:02 +0700

    Aplikasi ATM
    project visual studio 2015

---
+++ AplikasiATM/Nasabah.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiATM
+{
+    class Nasabah
+    {
+        int idNasabah;
+        string nama;
+        string gender;
+        string noRekening;
+        double saldo;
+        int pin;
+        public Nasabah()
+        {
+
+        }
+        public Nasabah(int id, string n, string g, string nr, double s, int p )
+        {
+            this.idNasabah = id;
+            this.nama = n;
+            this.gender = g;
+            this.noRekening = nr;
+            this.saldo = s;
+            this.pin = p;
+        }
+
+        public int getId()
+        {
+            return idNasabah;
+        }
+
+        public int getPin()
+        {
+            return pin;
+        }
+
+        public double getSaldo()
+        {
+            return saldo;
+        }
+
+        public string getNoRekening()
+        {
+            return noRekening;
+        }
+
+        public int cariNasabah(int id)
+        {
+
+            do
+            {
+                if (id == this.idNasabah)
+                {
+                    Console.WriteLine(this.idNasabah);
+                }
+                else
+                {
+                    Console.WriteLine("Wrong");
+                }
+            }
+            while (id != this.idNasabah);
+            return idNasabah;
+        }
+
+        public void TampilData()
+        {
+            Console.WriteLine("Nama : "+this.nama);
+            Console.WriteLine("Jenis Kelamin : "+this.gender);
+            Console.WriteLine("No Rekening : "+this.noRekening);
+            
+        }
+
+        
+        public double cekSaldo()
+        {
+            Console.WriteLine("Saldo saat ini: "+this.saldo);
+            return saldo;
+        }
+        
+        public double saldoTambah(double nominal)
+        {
+            double saldoTambah = saldo + nominal;
+            this.saldo = saldoTambah;
+            return saldo;
+        }
+
+        public double saldoBerkurang(double nominal)
+        {
+            double saldoTambah = saldo - nominal;
+            this.saldo = saldoTambah;
+            return saldo;
+        }
+        /**
+        public int tambahSaldo()
+        {
+
+        }
+
+        **/
+    }
+}
---
+++ AplikasiATM/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiATM
+{
+    class Program
+    {
+        public static int pilihan;
+        static void Main(string[] args)
+        {
+            Program menu = new Program();
+            
+            Console.WriteLine("Selamat Datang di Aplikasi ATM");
+            //Nasabah(id,"nama","gender",'norekening',saldo,pin)
+            Nasabah all = new Nasabah();
+            Nasabah n1 = new Nasabah(203, "Muhammad Natsir", "L", "003214567", 1000, 1323);//set data
+            Nasabah n2 = new Nasabah(204, "M Agus Arianto", "L", "003212311", 1000, 2222);//set data
+            Awal:
+            Console.Write("Masukan id Account : ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukan Pin anda : ");
+            int pin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            PilihanMenu :
+            
+            if (id == n1.getId() && pin == n1.getPin())
+            {
+                menu.menu();
+                if (pilihan == 1)
+                {
+                    n1.cekSaldo();
+                    goto PilihanMenu;
+                }
+                else if (pilihan == 2)
+                {
+                    double saldo;
+                    Console.Write("Masukkan nomer rekening tujuan : ");
+                    string noRekTujuan = Console.ReadLine();
+
+                    if (noRekTujuan == n2.getNoRekening())
+                    {
+                        Console.Write("Masukkan Jumlah uang yang dikirim : ");
+                        double nominal = Convert.ToInt32(Console.ReadLine());
+
+                        n1.saldoBerkurang(nominal);
+                        n2.saldoTambah(nominal);
+                        n1.cekSaldo();
+
+                        goto PilihanMenu;
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("Maaf nomer yanga anda masukkan salah");
+                        goto PilihanMenu;
+                    }
+
+                }
+                else if (pilihan == 3)
+                {
+                    Console.Write("Masukkan Jumlah uang yang di Tambah : ");
+                    double nominal = Convert.ToInt32(Console.ReadLine());
+                    n1.saldoTambah(nominal);
+                    n1.cekSaldo();
+                    goto PilihanMenu;
+                }
+                else if (pilihan == 4)
+                {
+                    Console.WriteLine("Terimakasih");
+                    Environment.Exit(0);
+                }
+            }
+            else if(id == n2.getId() && pin == n2.getPin())
+            {
+                menu.menu();
+                if (pilihan == 1)
+                {
+                    n1.cekSaldo();
+                    goto PilihanMenu;
+                }
+                else if (pilihan == 2)
+                {
+                    double saldo;
+                    Console.Write("Masukkan nomer rekening tujuan : ");
+                    string noRekTujuan = Console.ReadLine();
+
+                    if (noRekTujuan == n1.getNoRekening())
+                    {
+                        Console.Write("Masukkan Jumlah uang yang dikirim : ");
+                        double nominal = Convert.ToInt32(Console.ReadLine());
+
+                        n2.saldoBerkurang(nominal);
+                        n1.saldoTambah(nominal);
+                        n2.cekSaldo();
+
+                        goto PilihanMenu;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Maaf nomer yanga anda masukkan salah");
+                        goto PilihanMenu;
+                    }
+
+                }
+                else if (pilihan == 3)
+                {
+                    Console.Write("Masukkan Jumlah uang yang di Tambah : ");
+                    double nominal = Convert.ToInt32(Console.ReadLine());
+                    n2.saldoTambah(nominal);
+                    n2.cekSaldo();
+                    goto PilihanMenu;
+                }
+                else if (pilihan == 4)
+                {
+                    Console.WriteLine("Terimakasih");
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Maaf Id atau Pin yang anda masukkan salah");
+                goto Awal;
+            }
+
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        public void menu()
+        {
+            Console.WriteLine("Piliha Menu : ");
+            Console.WriteLine("1. Cek Saldo");
+            Console.WriteLine("2. Kirim Uang");
+            Console.WriteLine("3. Tambah Saldo");
+            Console.WriteLine("4. Keluar");
+            Console.Write("Masukan pilihan : ");
+            pilihan = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+        }
+    }
+}
